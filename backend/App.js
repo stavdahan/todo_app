485d93950b337e052664576ec6da29587dc65630
Stav Dahan <stav0405@gmail.com>
2023-01-07 10:12:35 +0200

    routes fixed

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const port = 4000;
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- backend/controllers/todosController.js
+++ backend/controllers/todosController.js
@@ -1,49 +1,72 @@
-const fs = require('fs')
-const data = fs.readFileSync('./db.json')
-const todos = JSON.parse(data)
+const fs = require("fs");
+const data = fs.readFileSync("./db.json");
+const todos = JSON.parse(data);
+const { v4: uuidv4 } = require("uuid");
 
 const getTodos = (req, res) => {
-    res.status(200).json(todos)
-}
+  res.status(200).json(todos);
+};
 
-const getTodo = (req, res) => { 
-    todo = todos.find(todo => todo.id == req.params.id)
-    todo ? res.status(200).json(todo) : res.status(200).json({err: 'cant find the todo'})
-}
+const getTodo = (req, res) => {
+  todo = todos.find((todo) => todo.id == req.params.id);
+  todo
+    ? res.status(200).json(todo)
+    : res.status(200).json({ err: "cant find the todo" });
+};
 
-const setTodo = (req, res) => { 
-    new_todo = req.body
-    if (checkTodoIsCorrect(new_todo)) {
-        todos.push(new_todo)
-        fs.writeFileSync('./db.json', JSON.stringify(todos))
-    } else {
+const setTodo = (req, res) => {
+  todoText = req.body.text;
+  id = uuidv4();
+  if (checkTodoIsCorrect(todoText)) {
+    const newTodo = {
+      id,
+      text: todoText,
+      isCompleted: false,
+      date: Date.now(),
+    };
+    todos.push(newTodo);
+    fs.writeFileSync("./db.json", JSON.stringify(todos));
+  } else {
+    console.log("This todo exists in the DB");
+  }
+  res.status(200).json({ message: `create a new todo` });
+};
 
-     }
-    res.status(200).json({ message: `create a new todo` })
-}
-
-const updateTodo = (req, res) => { 
-    res.status(200).json({ message: `updated todo number: ${req.params.id}`})
-}
-
-const deleteTodo = (req, res) => { 
-    res.status(200).json({ message: `deleted todo number ${req.params.id}`})
-}
-
-
-const checkTodoIsCorrect = ({ id, text, isCompleted, date }) => { 
-    console.log(id, text, isCompleted, date)
-    if (typeof text == 'string' && typeof date == 'number') {
-        return true;
-    } else {
-        return false;
+const updateTodo = (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  for (const todo of todos) {
+    if (todo.id == id) {
+      todo.isCompleted = !todo.isCompleted;
     }
-}
+  }
+  fs.writeFileSync("./db.json", JSON.stringify(todos));
+  res.status(200).json({ message: `updated todo number: ${req.params.id}` });
+};
+
+const deleteTodo = (req, res) => {
+  res.status(200).json({ message: `deleted todo number ${req.params.id}` });
+};
+
+const checkTodoIsCorrect = (text) => {
+  let isAvailable = !checkTodoIsAvailable(text);
+  console.log(typeof text);
+  console.log(isAvailable);
+  if (typeof text == "string" && isAvailable) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkTodoIsAvailable = (text) => {
+  todos.find((todo) => todo.text == text);
+};
 
 module.exports = {
-    getTodos,
-    getTodo,
-    setTodo,
-    updateTodo,
-    deleteTodo,
-}
+  getTodos,
+  getTodo,
+  setTodo,
+  updateTodo,
+  deleteTodo,
+};
